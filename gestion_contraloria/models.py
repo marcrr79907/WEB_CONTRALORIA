c44364608d27b09727ecf394f4d005a6a24fbfad
marcrr79907 <allmost79907@gmail.com>
2024-06-09 11:04:47 -0500

    models

--- gestion_contraloria/models.py
+++ gestion_contraloria/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from datetime import datetime
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Organizacion(models.Model):
+    
+    nombre = models.CharField(max_length=50, null=False, unique=True)
+    descripcion = models.CharField(max_length=250,)
+    direccion = models.CharField(max_length=250,)
+    telefono = models.CharField(max_length=20,)
+    
+
+class Presupuesto(models.Model):
+    
+    año = models.IntegerField(null=False)
+    monto_total = models.DecimalField(max_digits=15, decimal_places=2)
+    estado = models.CharField(max_length=20)
+
+    organizacion_id = models.ForeignKey(Organizacion, on_delete=models.SET_NULL)
+
+class Transaccion(models.Model):
+    
+    fecha_transaccion = models.DateTimeField(auto_now_add=True, default=datetime.now)
+    monto = models.DecimalField(max_digits=15, decimal_places=2)
+    descripcion = models.CharField(max_length=250,)
+
+    id_presupuesto = models.ForeignKey(Presupuesto, on_delete=models.SET_NULL)
+
+class Auditoria(models.Model):
+    
+    fecha_inicio = models.DateTimeField(auto_now_add=True, default=datetime.now)
+    fecha_fin = models.DateTimeField(auto_now_add=True, default=datetime.now)
+    estado = models.CharField(max_length=20)
+
+    organizacion_id = models.ForeignKey(Organizacion, on_delete=models.SET_NULL)
+    id_auditor = models.ForeignKey(User, on_delete=models.SET_NULL)
+
+class Hallazgo (models.Model):
+    
+    descripcion = models.CharField(max_length=250,)
+    recomendacion = models.CharField(max_length=250,)
+    estado = models.CharField(max_length=20)
+
+    id_auditoria = models.ForeignKey(Auditoria, on_delete=models.SET_NULL)
+
+class InformesFinancieros(models.Model):
+    pass
