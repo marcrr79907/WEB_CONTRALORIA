aaa552862af0740bc0ac1d78c5a5ab23096dce8a
marcrr79907 <allmost79907@gmail.com>
2024-06-17 20:53:59 -0400

    login

--- gestion_contraloria/models.py
+++ gestion_contraloria/models.py
@@ -1,23 +1,30 @@
-# from django.db import models
-# from datetime import datetime
-# from user.models import User
+from django.db import models
+from django.utils.timezone import now
+from user.models import User
 
-# class Organizacion(models.Model):
+class Organizacion(models.Model):
     
-#     nombre = models.CharField(max_length=50, null=False, unique=True)
-#     descripcion = models.CharField(max_length=250,)
-#     direccion = models.CharField(max_length=250,)
-#     telefono = models.CharField(max_length=20,)
+    nombre = models.CharField(max_length=50, null=False, unique=True)
+    descripcion = models.CharField(max_length=250,)
+    direccion = models.CharField(max_length=250,)
+    telefono = models.CharField(max_length=20,)
     
 
-# class Presupuesto(models.Model):
-    
-#     fecha = models.DateField(default=datetime.now())
-#     monto_total = models.DecimalField(max_digits=15, decimal_places=2)
-#     estado = models.CharField(max_length=20)
+class Reporte(models.Model):
 
-#     organizacion_id = models.ForeignKey(Organizacion, on_delete=models.PROTECT)
-#     user_id = models.ForeignKey(User, on_delete=models.PROTECT)
+    
+
+    organizacion_id = models.ForeignKey(Organizacion, on_delete=models.PROTECT)
+
+class Presupuesto(models.Model):
+    
+    fecha = models.DateField(auto_now=True)
+    monto_total = models.DecimalField(max_digits=15, decimal_places=2)
+    estado = models.CharField(max_length=20)
+
+    organizacion_id = models.ForeignKey(Organizacion, on_delete=models.PROTECT)
+    user_id = models.ForeignKey(User, on_delete=models.PROTECT)
+    reporte_id = models.ForeignKey(Reporte, on_delete=models.PROTECT)
 
 # class Transaccion(models.Model):
     
@@ -26,25 +33,7 @@
 #     descripcion = models.CharField(max_length=250,)
 
 #     id_presupuesto = models.ForeignKey(Presupuesto, on_delete=models.PROTECT)
-# ##################################################################################
-# class Auditoria(models.Model):
+
+##################################################################################
+
     
-#     fecha_inicio = models.DateTimeField(auto_now_add=True)
-#     fecha_fin = models.DateTimeField(auto_now_add=True)
-#     estado = models.CharField(max_length=20)
-
-#     organizacion_id = models.ForeignKey(Organizacion, on_delete=models.PROTECT)
-#     id_auditor = models.ForeignKey(User, on_delete=models.PROTECT)
-
-
-# class Hallazgo (models.Model):
-    
-#     descripcion = models.CharField(max_length=250,)
-#     recomendacion = models.CharField(max_length=250,)
-#     estado = models.CharField(max_length=20)
-
-#     id_auditoria = models.ForeignKey(Auditoria, on_delete=models.PROTECT)
-# #########################################################################
-
-# class InformesFinancieros(models.Model):
-#     pass
