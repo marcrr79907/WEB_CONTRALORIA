1bacaac1c1f1b5438d35a397fd3d6776e13ef2e5
marcrr79907 <allmost79907@gmail.com>
2024-07-01 07:44:35 -0400

    DONE

--- gestion_contraloria/views/dashboard.py
+++ gestion_contraloria/views/dashboard.py
@@ -1,33 +1,24 @@
-from django.shortcuts import redirect
-
-from ..mixins import IsSuperuserMixin
-from django.views.generic import TemplateView, View
+from django.shortcuts import redirect, render
+from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from ..models import *
+from ..models import Reporte
 from datetime import datetime
 
-
-class DashboardView(LoginRequiredMixin, IsSuperuserMixin, TemplateView):
-
+class DashboardView(LoginRequiredMixin, TemplateView):
     template_name = 'dashboard/dashboard.html'
 
     def dispatch(self, request, *args, **kwargs):
         if request.user.is_superuser:
             return redirect('/admin/')
         user = request.user
-        print(type(user.groups.all()[0]))
-        if user.groups.all():
-            if str(user.groups.all()[0]) == 'Auditor':
-
-                return redirect('gestion_contraloria:organization_list')
-
+        if user.groups.all() and str(user.groups.all()[0]) == 'Auditor':
+            return redirect('gestion_contraloria:organization_list')
         return super().dispatch(request, *args, **kwargs)
 
-    def get_reports_year_month(self):
+    def get_reports_year_month(self, year):
         data = []
         try:
-            year = datetime.now().year
-            for m in range(1, 13):
+            for m in range(1, 12):
                 reportes = Reporte.objects.filter(fecha__year=year, fecha__month=m).count()
                 data.append(reportes)
         except:
@@ -35,8 +26,10 @@ class DashboardView(LoginRequiredMixin, IsSuperuserMixin, TemplateView):
         return data
 
     def get_context_data(self, **kwargs):
-
-
         context = super().get_context_data(**kwargs)
-        context['reportes_anno_mes'] = self.get_reports_year_month()
+        year = self.request.GET.get('year', datetime.now().year)
+
+        context['selected_year'] = int(year)
+        context['reportes_anno_mes'] = self.get_reports_year_month(year)
+        context['years'] = range(datetime.now().year - 5, datetime.now().year + 1)
         return context
